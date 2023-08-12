cf3f9301e7436bfd31dc5a7cb264714d9b9d91e1
IOANNIS IASON BOIDANIS <61283674+JasonSDMN2001@users.noreply.github.com>
2023-08-12 17:58:42 +0300

    hnj

---
+++ AAY_lights/AAY_lights/Form1.Designer.cs
@@ -0,0 +1,152 @@
+﻿namespace AAY_lights
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.panel1 = new System.Windows.Forms.Panel();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.panel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(161)));
+            this.label1.Location = new System.Drawing.Point(27, 51);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(165, 29);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Choose room:";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(161)));
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Items.AddRange(new object[] {
+            "Bedroom1",
+            "Bedroom2",
+            "WC",
+            "Kitchen",
+            "Living Room"});
+            this.comboBox1.Location = new System.Drawing.Point(187, 51);
+            this.comboBox1.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(229, 37);
+            this.comboBox1.TabIndex = 1;
+            this.comboBox1.SelectedIndexChanged += new System.EventHandler(this.comboBox1_SelectedIndexChanged);
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(192)))), ((int)(((byte)(0)))));
+            this.button1.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.button1.Enabled = false;
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(161)));
+            this.button1.ForeColor = System.Drawing.Color.White;
+            this.button1.Location = new System.Drawing.Point(418, 4);
+            this.button1.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(73, 69);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "ON";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.BackColor = System.Drawing.Color.Red;
+            this.button2.Enabled = false;
+            this.button2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(161)));
+            this.button2.Location = new System.Drawing.Point(497, 4);
+            this.button2.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(78, 69);
+            this.button2.TabIndex = 3;
+            this.button2.Text = "OFF";
+            this.button2.UseVisualStyleBackColor = false;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = global::AAY_lights.Properties.Resources.wc_off;
+            this.pictureBox1.Location = new System.Drawing.Point(0, 0);
+            this.pictureBox1.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(412, 391);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 4;
+            this.pictureBox1.TabStop = false;
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.pictureBox1);
+            this.panel1.Controls.Add(this.button2);
+            this.panel1.Controls.Add(this.button1);
+            this.panel1.Location = new System.Drawing.Point(33, 126);
+            this.panel1.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(578, 401);
+            this.panel1.TabIndex = 5;
+            this.panel1.Visible = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(628, 562);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.label1);
+            this.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Manage Ligths";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.panel1.ResumeLayout(false);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Panel panel1;
+    }
+}
+
---
+++ AAY_lights/AAY_lights/Form1.cs
@@ -0,0 +1,163 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace AAY_lights
+{
+    public partial class Form1 : Form
+    {
+        int bedroom1_light = 0, bedroom2_light = 0, kitchen_light = 0, wc_light = 0, living_light = 0;
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            if (comboBox1.Text == "Bedroom1")
+            {
+                pictureBox1.Image = Properties.Resources.bedroom1_off;
+                bedroom1_light = 0;
+            }
+            else if (comboBox1.Text == "Bedroom2")
+            {
+                pictureBox1.Image = Properties.Resources.bedroom2_off;
+                bedroom2_light = 0;
+            }
+            else if (comboBox1.Text == "Kitchen")
+            {
+                pictureBox1.Image = Properties.Resources.kitchen_off;
+                kitchen_light = 0;
+            }
+            else if (comboBox1.Text == "WC")
+            {
+                pictureBox1.Image = Properties.Resources.wc_off;
+                wc_light = 0;
+            }
+            else if (comboBox1.Text == "Living Room")
+            {
+                pictureBox1.Image = Properties.Resources.living_room_off;
+                living_light = 0;
+            }
+            button2.Enabled = false;
+            button1.Enabled = true;
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (comboBox1.Text == "Bedroom1")
+            {
+                pictureBox1.Image = Properties.Resources.bedroom1;
+                bedroom1_light = 1;
+            }
+            else if (comboBox1.Text == "Bedroom2")
+            {
+                pictureBox1.Image = Properties.Resources.bedroom2;
+                bedroom2_light = 1;
+            }
+            else if (comboBox1.Text == "Kitchen")
+            {
+                pictureBox1.Image = Properties.Resources.kitchen;
+                kitchen_light = 1;
+            }
+            else if (comboBox1.Text == "WC")
+            {
+                pictureBox1.Image = Properties.Resources.wc;
+                wc_light = 1;
+            }
+            else if (comboBox1.Text == "Living Room")
+            {
+                pictureBox1.Image = Properties.Resources.living_room;
+                living_light = 1;
+            }
+            button1.Enabled = false;
+            button2.Enabled = true;
+        }
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            panel1.Visible = true;
+            if (comboBox1.Text == "Bedroom1")
+            { if (bedroom1_light == 1)
+                {
+                    pictureBox1.Image = Properties.Resources.bedroom1;
+                    button2.Enabled = true;
+                }
+            else if(bedroom1_light == 0)
+                {
+                    pictureBox1.Image = Properties.Resources.bedroom1_off;
+                    button1.Enabled = true;
+                }
+            }
+            else if(comboBox1.Text == "Bedroom2")
+            {
+                if (bedroom2_light == 1)
+                {
+                    pictureBox1.Image = Properties.Resources.bedroom2;
+                    button2.Enabled = true;
+                }
+                else if (bedroom2_light == 0)
+                {
+                    pictureBox1.Image = Properties.Resources.bedroom2_off;
+                    button1.Enabled = true;
+                }
+            }
+            else if (comboBox1.Text == "Kitchen")
+            {
+                if (kitchen_light == 1)
+                {
+                    pictureBox1.Image = Properties.Resources.kitchen;
+                    button2.Enabled = true;
+                }
+                else if (kitchen_light == 0)
+                {
+                    pictureBox1.Image = Properties.Resources.kitchen_off;
+                    button1.Enabled = true;
+                }
+            }
+            else if (comboBox1.Text == "WC")
+            {
+                if (wc_light == 1)
+                {
+                    pictureBox1.Image = Properties.Resources.wc;
+                    button2.Enabled = true;
+                }
+                else if (wc_light == 0)
+                {
+                    pictureBox1.Image = Properties.Resources.wc_off;
+                    button1.Enabled = true;
+                }
+            }
+            else if (comboBox1.Text == "Living Room")
+            {
+                if (living_light == 1)
+                {
+                    pictureBox1.Image = Properties.Resources.living_room;
+                    button2.Enabled = true;
+                }
+                else if (living_light == 0)
+                {
+                    pictureBox1.Image = Properties.Resources.living_room_off;
+                    button1.Enabled = true;
+                }
+            }
+        }
+    }
+}
